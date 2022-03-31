77d28b0e8353fa3da76260b604fca1cfc1f2ab3f
dino <dino.zhao@outlook.com>
2022-03-31 14:52:55 +0800

    fix

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,15 +3,20 @@ import { MemoryRouter as Router } from 'react-router-dom';
 import { render } from 'react-dom';
 import zhCN from 'antd/lib/locale/zh_CN';
 import { ConfigProvider } from 'antd';
+import { Provider } from 'react-redux';
 import App from './App';
 import 'moment/locale/zh-cn';
 import 'antd/dist/antd.css';
 
+import { store } from './store';
+
 render(
   <ConfigProvider locale={zhCN}>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </ConfigProvider>,
   document.getElementById('root')
 );
