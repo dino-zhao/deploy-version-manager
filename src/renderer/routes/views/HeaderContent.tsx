d9a0f689c55be0378f25f684c88ed02bcf4ea958
dino <dino.zhao@outlook.com>
2022-04-01 10:14:43 +0800

    fix

--- src/renderer/routes/views/HeaderContent.tsx
+++ src/renderer/routes/views/HeaderContent.tsx
@@ -1,13 +1,8 @@
 import styled from 'styled-components';
 import { SettingTwoTone } from '@ant-design/icons';
-import { Modal } from 'antd';
-import { useState } from 'react';
-import {
-  useAppSelector,
-  selectIsInit,
-  useAppDispatch,
-  initOss,
-} from 'renderer/store';
+import { message, Modal } from 'antd';
+import { useState, useEffect } from 'react';
+import { useAppSelector, useAppDispatch, initOss } from 'renderer/store';
 import { initOssClient } from 'renderer/util';
 import OssConfig from './OssConfig';
 
@@ -20,19 +15,24 @@ const Wrap = styled.div`
 
 export default function HeaderContent() {
   const [visibel, setVisile] = useState(false);
-  const isInit = useAppSelector(selectIsInit);
+  const { config } = useAppSelector((state) => state.ossConfig);
   const dispatch = useAppDispatch();
-  const localAk = localStorage.getItem('ak');
-  if (!localAk) {
+  if (!localStorage.getItem('ak')) {
     setVisile(true);
-  } else if (!isInit) {
-    initOssClient(JSON.parse(localAk))
-      .then((res) => {
-        dispatch(initOss());
-        return res;
-      })
-      .catch((err) => console.log(err));
   }
+  useEffect(() => {
+    if (config.accessKeyId) {
+      initOssClient(config)
+        .then((res) => {
+          dispatch(initOss());
+          return res;
+        })
+        .catch(() => {
+          message.error('初始化失败');
+        });
+    }
+  }, [config, dispatch]);
+
   return (
     <>
       <Wrap>
